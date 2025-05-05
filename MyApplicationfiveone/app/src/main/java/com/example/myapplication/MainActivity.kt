a4dbc9cba42050fe9e7dec80263842ca2b998e46
joker <1206897770@qq.com>
2025-05-05 23:10:03 +0800

    添加了倒计时测试

--- MyApplicationfiveone/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplicationfiveone/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,17 +2,17 @@ package com.example.myapplication
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var  btnSendImmediate:Button
-    private lateinit var btnSendDelayed:Button
+    private lateinit var  mbtnSendImmediate:Button
+    private lateinit var mbtnSendDelayed:Button
     private lateinit var mtvMessage: TextView
     private lateinit var handler: Handler
+    private lateinit var mbtnCount:Button
+    private var countSecond=10
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,14 +23,22 @@ class MainActivity : AppCompatActivity() {
 
 
     }
-    fun initView(){
+    private fun initView(){
         mtvMessage = findViewById(R.id.tv_message_log)
-        btnSendImmediate = findViewById<Button>(R.id.btn_send_immediate)
-        btnSendDelayed = findViewById<Button>(R.id.btn_send_delayed)
+        mbtnSendImmediate = findViewById(R.id.btn_send_immediate)
+        mbtnSendDelayed = findViewById(R.id.btn_send_delayed)
+        mbtnCount=findViewById(R.id.btn_count)
     }
-    fun initEvent(){
-        startRecive()
-        btnSendImmediate.setOnClickListener {
+    private fun initEvent(){
+
+
+        mbtnCount.setOnClickListener{
+            val msg= Message.obtain()
+            countSecond=10
+            msg.what=2
+            handler.sendMessageDelayed(msg,1000)
+        }
+        mbtnSendImmediate.setOnClickListener {
             val msg = Message.obtain()
             msg.what = 1
             msg.obj="即时消息"
@@ -38,25 +46,44 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-        btnSendDelayed.setOnClickListener {
+        mbtnSendDelayed.setOnClickListener {
             val msg = Message.obtain()
-            msg.what = 2
+            msg.what = 1
             msg.obj="延时消息"
             handler.sendMessageDelayed(msg, 1000)
         }
+        startRecive()
 
     }
-    fun startRecive(){
+    private fun startRecive(){
         Thread {
             Looper.prepare()
 
             handler = object : Handler() {
                 override fun handleMessage(msg: Message) {
+                    val newMsg=Message()
+                    newMsg.what=2
                     val what=msg.what
                     val obj=msg.obj
-                    runOnUiThread {
-                        mtvMessage.append("收到消息: $what$obj\n")
+                    when(what){
+                        1->runOnUiThread {
+                            mtvMessage.append("收到消息: $what$obj\n")
+                        }
+                        2-> if (countSecond>0){
+                            runOnUiThread {
+                                mtvMessage.append("倒计时:$countSecond\n")
+                            }
+                            countSecond--
+                            handler.sendMessageDelayed(newMsg,1000)
+
+                        }else{
+                           runOnUiThread {
+                               mtvMessage.append("倒计时结束\n")
+                           }
+
+                        }
                     }
+
                 }
             }
             Looper.loop()
